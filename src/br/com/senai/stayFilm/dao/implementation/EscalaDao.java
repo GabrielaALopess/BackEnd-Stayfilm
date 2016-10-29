919cde5c9a86cd9c199d7e286138ebe7c85438ce
Gabriela Almeida <Gabriela Almeida>
2016-10-28 22:35:41 -0200

    Escala simples, comparecimento, data e  hora agendada.

--- src/br/com/senai/stayFilm/dao/implementation/EscalaDao.java
+++ src/br/com/senai/stayFilm/dao/implementation/EscalaDao.java
@@ -3,48 +3,63 @@ package br.com.senai.stayFilm.dao.implementation;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.senai.stayFilm.dao.GenericDao;
+import br.com.senai.stayFilm.model.Colaborador;
 import br.com.senai.stayFilm.model.Escala;
 
 @Repository
 public class EscalaDao implements GenericDao<Escala> {
 
+	@PersistenceContext
+	private EntityManager manager;
+
 	@Override
 	public void insert(Escala obj) throws SQLException {
-		// TODO Auto-generated method stub
+		// NOT USE
 
 	}
 
+	@Transactional
 	@Override
-	public void delete(Long idObj) throws SQLException {
-		// TODO Auto-generated method stub
+	public void delete(Long idEscala) throws SQLException {
+		Escala escala = manager.find(Escala.class, idEscala);
+		manager.remove(escala);
 
 	}
 
 	@Override
 	public Escala update(Escala obj) throws SQLException {
-		// TODO Auto-generated method stub
+		// NOT USE
 		return null;
 	}
 
 	@Override
-	public Escala search(Long idObj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void insertWithKey(Escala obj, long idColaborador) {
-		// TODO Auto-generated method stub
+	public Escala search(Long idEscala) {
+		return manager.find(Escala.class, idEscala);
 
 	}
 
+	@Transactional
 	@Override
-	public Escala updateWithKey(Escala obj, long idObj) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public void insertWithKey(Escala escala, long idColaborador) {
+		Colaborador colaborador = manager.find(Colaborador.class, idColaborador);
+		escala.setIdColaborador(colaborador);
+		manager.persist(escala);
+	}
+
+	@Transactional
+	@Override
+	public Escala updateWithKey(Escala escala, long idColaborador) throws SQLException {
+		Colaborador colaborador = manager.find(Colaborador.class, idColaborador);
+		escala.setIdColaborador(colaborador);
+		return manager.merge(escala);
+
 	}
 
 	@Override
