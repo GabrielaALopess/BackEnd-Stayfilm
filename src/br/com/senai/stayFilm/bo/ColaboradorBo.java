aa94172ef6cd01ca4a5264df9ebee68826812892
Gabriela Almeida <Gabriela Almeida>
2016-11-05 09:44:33 -0200

    Insert de colaborador funcionando

---
+++ src/br/com/senai/stayFilm/bo/ColaboradorBo.java
@@ -0,0 +1,34 @@
+package br.com.senai.stayFilm.bo;
+
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import br.com.senai.stayFilm.dao.GenericDao;
+import br.com.senai.stayFilm.model.Colaborador;
+
+/**
+ * Bo de Colaborador
+ * @author Equipe StayFilm
+ *
+ */
+
+@Component
+public class ColaboradorBo {
+	
+	@Autowired
+	@Qualifier("colaboradorDao")
+	private GenericDao<Colaborador> colaboradorDao;
+
+	/**
+	 * Método para inserir um colaborador
+	 * @param colaborador
+	 * @throws SQLException
+	 */
+	public void insert(Colaborador colaborador) throws SQLException{
+		colaboradorDao.insert(colaborador);
+	}
+}
--- src/br/com/senai/stayFilm/controller/ColaboradorRestController.java
+++ src/br/com/senai/stayFilm/controller/ColaboradorRestController.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.senai.stayFilm.dao.GenericDao;
+import br.com.senai.stayFilm.bo.ColaboradorBo;
 import br.com.senai.stayFilm.model.Colaborador;
 
 /**
@@ -27,14 +26,13 @@ import br.com.senai.stayFilm.model.Colaborador;
 public class ColaboradorRestController {
 
 	@Autowired
-	@Qualifier("colaboradorDao")
-	private GenericDao<Colaborador> colaboradorDao;
+	public ColaboradorBo colaboradorBO;
 
 	@RequestMapping(value = "/colaborador", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<Colaborador> inserir(@RequestBody Colaborador colaborador) throws SQLException {
 
 		try {
-			colaboradorDao.insert(colaborador);
+			colaboradorBO.insert(colaborador);
 			URI location = new URI("/colaborador" + colaborador.getIdColaborador());
 			return ResponseEntity.created(location).body(colaborador);
 		} catch (URISyntaxException e) {
@@ -46,18 +44,20 @@ public class ColaboradorRestController {
 
 	@RequestMapping(value = "/colaborador/{idColaborador}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Colaborador pesquisarColaborador(@PathVariable Long idColaborador) throws SQLException {
-		return colaboradorDao.search(idColaborador);
+//		return colaboradorDao.search(idColaborador);
+		return null;
 	}
 
 	@RequestMapping(value = "/colaborador/editar/{idColaborador}", method = RequestMethod.PUT)
 	public Colaborador altera(@RequestBody Colaborador colaborador, @PathVariable long idColaborador)
 			throws SQLException {
-		return colaboradorDao.update(colaborador);
+//		return colaboradorDao.update(colaborador);
+	return null;
 	}
 
 	@RequestMapping(value = "/colaborador/{idColaborador}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> remover(@PathVariable long idColaborador) throws SQLException {
-		colaboradorDao.delete(idColaborador);
+//		colaboradorDao.delete(idColaborador);
 		return ResponseEntity.noContent().build();
 	}
 
