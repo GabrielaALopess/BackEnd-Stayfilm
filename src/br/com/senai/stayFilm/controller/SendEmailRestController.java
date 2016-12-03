628f742846d7fd2e478b61ce886398557f1b29ab
felipeacelino <felipeacelino@hotmail.com>
2016-12-03 15:48:39 -0200

    fix email

--- src/br/com/senai/stayFilm/controller/SendEmailRestController.java
+++ src/br/com/senai/stayFilm/controller/SendEmailRestController.java
@@ -3,11 +3,8 @@ package br.com.senai.stayFilm.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.senai.stayFilm.bo.SendEmaiBo;
-
 @CrossOrigin
 @RestController
 public class SendEmailRestController {
 
-	public SendEmaiBo sendEmailBo;
 }
